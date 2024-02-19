7d0457bf878773e91b658a820e38a57ec0fbe823
Dhiraj Phuyal <phuyal.dhiraj17@gmail.com>
2024-02-19 15:58:21 +0545

    refractor: remove unused routes and controllers

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const userRoutes = require('./routes/userRoutes');
-const decorationRoutes = require('./routes/decorationRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const decoratorRoutes = require('./routes/decoratorRoutes');
-const connectRoutes = require('./routes/connectRoutes');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const {createDecoration, getAllDecorations, getDecorationById, updateDecoration, deleteDecoration} = require("./controllers/decorationController");
-const {createBooking, getBookingsByCustomer, updateBookingStatus, cancelBooking} = require("./controllers/bookingController");
 const {registerUser, loginUser} = require("./controllers/userController");
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const orderRoutes = require("./routes/order");
+const esewaRoutes = require("./routes/esewa");
+const {createOrder, getAllOrders} = require("./order_controller");
+const {handleEsewaSuccess} = require("./controllers/esewaController");
+const {updateOrderAfterPayment} = require("./controllers/orderController");
+
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // for parsing application/json
 
 // routes
 app.use('/api/users', userRoutes);
-app.use('/api/decorations', decorationRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/decorators', decoratorRoutes);
-app.use('/api/connect', connectRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/esewa", esewaRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Connected to MongoDB')).catch(err => console.error('Could not connect to MongoDB', err));
@@ -34,15 +34,9 @@ app.get('/', (req, res) => {
 });
 app.post('/register', registerUser);
 app.post('/login', loginUser);
-app.post('/decorations', createDecoration);
-app.get('/decorations', getAllDecorations);
-app.get('/decorations/:id', getDecorationById);
-app.put('/decorations/:id', updateDecoration);
-app.delete('/decorations/:id', deleteDecoration);
-app.post('/bookings', createBooking);
-app.get('/bookings/customer/:customerId', getBookingsByCustomer);
-app.put('/bookings/:bookingId', updateBookingStatus);
-app.patch('/bookings/cancel/:bookingId', cancelBooking);
+app.post('/orders/create', createOrder);
+app.get("/orders", getAllOrders)
+app.get("/success", handleEsewaSuccess, updateOrderAfterPayment);
 
 app.use((err, req, res, next) => {
     console.log(err);
